c1e6adbde228fbd6b91ed7d269dd2570eb8f7cfe
ghost <ghost@local>
2026-03-14 15:51:32 +0000

    fix: require macOS 14+ for onChange API

--- SisyphusUI/Package.swift
+++ SisyphusUI/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SisyphusUI",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     targets: [
         .executableTarget(
             name: "SisyphusUI",
